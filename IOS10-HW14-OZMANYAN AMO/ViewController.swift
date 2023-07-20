b0da8104236510c6d7a3bba34d5722b8667d57c1
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-07-20 23:19:14 +0300

    Initial Commit

---
+++ IOS10-HW14-OZMANYAN AMO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS10-HW14-OZMANYAN AMO
+//
+//  Created by Amahastla on 20.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
