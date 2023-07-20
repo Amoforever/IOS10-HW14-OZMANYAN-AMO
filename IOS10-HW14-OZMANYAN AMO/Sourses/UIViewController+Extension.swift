816a9df71970f7af4064fd1ebf6816076bb48e18
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-07-20 23:41:40 +0300

    Write the ViewController.

--- IOS10-HW14-OZMANYAN AMO/Sourses/UIViewController+Extension.swift
+++ IOS10-HW14-OZMANYAN AMO/Sourses/UIViewController+Extension.swift
@@ -5,4 +5,15 @@
 //  Created by Amahastla on 20.07.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func addSubviewsForAutoLayout(_ views: [UIView]) {
+        for view in views {
+            view.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(view)
+        }
+    }
+}
+
